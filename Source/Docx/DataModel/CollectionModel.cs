c62ec8b955ef53b10581fa4207e7a42743a7c509
Michal Strbka <Michal.Strbka@zuehlke.com>
2025-07-09 14:39:03 +0200

    Added default variables "empty" and "notEmpty" to collectionModel

--- Source/Docx/DataModel/CollectionModel.cs
+++ Source/Docx/DataModel/CollectionModel.cs
@@ -6,6 +6,8 @@ public sealed class CollectionModel : Model
 {
     private readonly Model[] _items;
     private readonly ValueModel _length;
+    private readonly ConditionModel _empty;
+    private readonly ConditionModel _notEmpty;
 
     public CollectionModel(IEnumerable<Model> items)
     {
@@ -15,17 +17,34 @@ public sealed class CollectionModel : Model
             item.Parent = this;
         }
 
-        _length = new ValueModel($"{_items.Length}");
-        _length.Parent = this;
+        _length = new ValueModel($"{_items.Length}")
+        {
+            Parent = this
+        };
+
+        _empty = new ConditionModel(_items.Length == 0)
+        {
+            Parent = this
+        };
+
+        _notEmpty = new ConditionModel(_items.Length > 0)
+        {
+            Parent = this
+        };
     }
 
     public IEnumerable<Model> Items => _items;
 
     internal override Model Find(ModelExpression expression, string thisCharacter)
     {
-        if(expression.Root == "length")
+        switch (expression.Root)
         {
-            return _length;
+            case "length":
+                return _length;
+            case "empty":
+                return _empty;
+            case "notEmpty":
+                return _notEmpty;
         }
 
         if(expression.Root == thisCharacter)
